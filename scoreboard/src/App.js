6d256b0da7f2b52cf3e23026e266b3032da702e1
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-06-25 12:08:09 -0700

    MVP complete

--- scoreboard/src/App.js
+++ scoreboard/src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [strikes, setStrikes] = useState(0);
+  const [balls, setBalls] = useState(0);
+  const [fouls, setFouls] = useState(0);
+  const [hits, setHits] = useState(0);
+
+  useEffect(() => {
+    setBalls(0);
+    setStrikes(0);
+  }, [hits])
+
+  useEffect(() => {
+    strikes < 2 && fouls > 0
+    ? setStrikes(strikes + 1)
+    : setStrikes(strikes)
+  }, [fouls])
+
+  const addStrikes = e => {
+    strikes < 3
+    ? setStrikes(strikes + 1)
+    : setStrikes(0)
+  }
+
+  const addBalls = e => {
+    balls < 4
+    ? setBalls(balls + 1)
+    : setBalls(0)
+  }
+
+  const addHits = e => {
+    setHits(hits + 1)
+  }
+
+  const addFouls = e => {
+    setFouls(fouls + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h2>Strikes: { strikes } 
+      <button onClick={ addStrikes }>+</button></h2>
+
+      <h2>Balls: { balls } 
+      <button onClick={ addBalls }>+</button></h2>
+
+      <h2>Fouls: { fouls } 
+      <button onClick={ addFouls }>+</button></h2>
+
+      <h2>Hits: { hits } 
+      <button onClick={ addHits }>+</button></h2>
+
     </div>
   );
 }
--- scoreboard/src/App.test.js
+++ scoreboard/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+import App, { addStrike } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<App />, div);
+      ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders without errors', () => {
+    const queries = render(<App />);
+  });
+  
 });
